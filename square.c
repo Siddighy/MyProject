0920a586a83da0022f7a9034542e4d7eed6faada
Siddighy <siddighy63@gmail.com>
2018-12-08 00:29:50 +0100

    Code C ajouté pour le calcul du carré d'un entier

---
+++ square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+  printf("Enter a number, please\n");
+  int n;
+  scanf("%d",&n);
+  printf("its square is %d \n",n*n);
+  printf("The world of coders is beautiful \n")
+  return 0;
+}
