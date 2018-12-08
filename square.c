ce1b5e0ff6e778fc2e32ab7c1537060d9f290518
Siddighy <siddighy63@gmail.com>
2018-12-08 02:25:26 +0100

    Ajouté seulement surla branche test

--- square.c
+++ square.c
@@ -8,6 +8,10 @@ int main(){
   int n;
   scanf("%d",&n);
   printf("its square is %d \n",n*n);
-  printf("The world of coders is beautiful \n")
+  printf("The world of coders is beautiful \n");
+  printf("Enter another one, please\n");
+  int p;
+  scanf("%d",&p);
+  printf("it's square is %d \n",p*p);
   return 0;
 }
